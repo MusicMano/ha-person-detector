49a0faddef102a00733b992af85f27b44312eb96
root <root@a0d7b954-ssh.local.hass.io>
2025-02-02 21:20:20 -0500

    Simplify container setup

--- person-detector/run.sh
+++ person-detector/run.sh
@@ -1,12 +1,4 @@
-#!/usr/bin/with-contenv bashio
-
-# Get config values
-CAMERA_ENTITY=$(bashio::config 'camera_entity')
-CONFIDENCE_THRESHOLD=$(bashio::config 'confidence_threshold')
-SCAN_INTERVAL=$(bashio::config 'scan_interval')
+#!/bin/bash
 
 # Run the person detector
-python3 /person_detector.py \
-    --camera "${CAMERA_ENTITY}" \
-    --confidence "${CONFIDENCE_THRESHOLD}" \
-    --interval "${SCAN_INTERVAL}"
+exec python3 /app/person_detector.py
